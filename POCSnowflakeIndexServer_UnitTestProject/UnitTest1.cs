8e54d45b080665f30fee43c34e65fca90ceafa4e
Joseph Ross Cajulis <joseph2coc@gmail.com>
2026-01-12 22:37:32 +0800

    Added the unit test project. Initial values

---
+++ POCSnowflakeIndexServer_UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace POCSnowflakeIndexServer_UnitTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //setup here
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
