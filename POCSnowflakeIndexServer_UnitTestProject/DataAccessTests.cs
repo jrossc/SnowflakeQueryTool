9da1313624f6a11312656650ea7090b5517deb32
Joseph Ross Cajulis <joseph2coc@gmail.com>
2026-01-13 13:40:48 +0800

    Added unit tests for data access layer

--- POCSnowflakeIndexServer_UnitTestProject/DataAccessTests.cs
+++ POCSnowflakeIndexServer_UnitTestProject/DataAccessTests.cs
@@ -1,4 +1,5 @@
 ﻿using POCSnowflakeIndexServer.DAL;
+using System.Data;
 
 namespace POCSnowflakeIndexServer_UnitTestProject
 {
@@ -26,6 +27,42 @@ namespace POCSnowflakeIndexServer_UnitTestProject
         {
             Assert.That(_sut.ConnectionString, Is.EqualTo(
                 "Server=account.snowflakecomputing.com;User Id=test;Password=secret;"));
+        }
+
+        public void Connection_MustConnect()
+        {
+            string result = "";
+
+            result = _sut.TestConnection();
+
+            Assert.That(result.Contains("Connection Established"), Is.EqualTo(true));
+        }
+
+        public void Connection_MustNotConnect()
+        {
+            string result = "";
+
+            result = _sut.TestConnection();
+
+            Assert.That(result.Contains("Failed to establish"), Is.EqualTo(true));
+        }
+
+        public void GetData_IsNotNull()
+        {
+            DataTable result = new DataTable();
+
+            result = _sut.GetData("select * from table");
+
+            Assert.That(result.Rows.Count, Is.EqualTo(1));
+        }
+
+        public void GetDataJSON_IsNull()
+        {
+            string result = "";
+
+            result = _sut.GetDataJSON("select * from table");
+
+            Assert.That(result.Length, Is.GreaterThan(0));
         }
     }
 }
