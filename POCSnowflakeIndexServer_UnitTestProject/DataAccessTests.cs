617a98a8c9f607c4d3891c2e7e8aafd4abda1f3e
Joseph Ross Cajulis <joseph2coc@gmail.com>
2026-01-13 12:59:11 +0800

    added initial unit tests

---
+++ POCSnowflakeIndexServer_UnitTestProject/DataAccessTests.cs
@@ -0,0 +1,32 @@
+﻿using POCSnowflakeIndexServer.DAL;
+
+namespace POCSnowflakeIndexServer_UnitTestProject
+{
+    public class Tests
+    {
+        private SnowflakeDataAccess _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new SnowflakeDataAccess
+            {
+                ConnectionString = "Server=account.snowflakecomputing.com;User Id=test;Password=secret;"
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _sut?.Dispose();
+        }
+
+        [Test]
+        public void ConnectionString_IsInitialized_InSetUp()
+        {
+            Assert.That(_sut.ConnectionString, Is.EqualTo(
+                "Server=account.snowflakecomputing.com;User Id=test;Password=secret;"));
+        }
+    }
+}
+
